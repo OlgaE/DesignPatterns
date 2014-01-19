0334cea20fa2074770c4448e89f56483bae91b5f
OlgaE <olga.efimova.aol@gmail.com>
2014-01-19 11:13:14 +0700

    A simple example of "facade" pattern.

---
+++ DesignPatterns/src/pattern/facade/Circle.java
@@ -0,0 +1,11 @@
+package pattern.facade;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing a circle..");
+
+	}
+
+}
---
+++ DesignPatterns/src/pattern/facade/Rectangle.java
@@ -0,0 +1,11 @@
+package pattern.facade;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing a rectangle..");
+
+	}
+
+}
